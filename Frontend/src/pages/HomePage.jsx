8ca024372a13b43a6112d92dbe1caf9ad0d1ffed
Khusbu Patel <khusbu111patel555@gmail.com>
2026-09-07 18:19:52 +0530

    Rearranged sections in HomePage layout

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,11 +1,25 @@
 
+// import HeroSection from '../components/home/HeroSection';
+// import HowWeWorkSection from '../components/home/HowWeWorkSection';
+// import AboutSection from '../components/home/AboutSection';
+// import ServicesSection from '../components/home/ServicesSection';
+// import WhyChooseUsSection from '../components/home/WhyChooseUsSection';
+// import TeamSection from '../components/home/TeamSection';
+// import TestimonialsSection from '../components/home/TestimonialsSection';
+// import BlogSection from '../components/home/BlogSection';
+// import FaqSection from '../components/home/FaqSection';
+// import ContactSection from '../components/home/ContactSection';
+// import FloatingChatButton from '../components/home/FloatingChatButton';
+
+
+
 import HeroSection from '../components/home/HeroSection';
-import HowWeWorkSection from '../components/home/HowWeWorkSection';
 import AboutSection from '../components/home/AboutSection';
 import ServicesSection from '../components/home/ServicesSection';
+import HowWeWorkSection from '../components/home/HowWeWorkSection';
 import WhyChooseUsSection from '../components/home/WhyChooseUsSection';
-import TeamSection from '../components/home/TeamSection';
 import TestimonialsSection from '../components/home/TestimonialsSection';
+import TeamSection from '../components/home/TeamSection';
 import BlogSection from '../components/home/BlogSection';
 import FaqSection from '../components/home/FaqSection';
 import ContactSection from '../components/home/ContactSection';
@@ -15,12 +29,12 @@ const HomePage = () => {
   return (
     <div className="min-h-screen bg-[#0a0a1a] overflow-x-hidden">
       <HeroSection />
+        <AboutSection />
+         <ServicesSection />
       <HowWeWorkSection />
-      <AboutSection />
-      <ServicesSection />
       <WhyChooseUsSection />
-      <TeamSection />
       <TestimonialsSection />
+      <TeamSection />
       <BlogSection />
          <FaqSection />
       <ContactSection />
@@ -30,6 +44,5 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
 
 
